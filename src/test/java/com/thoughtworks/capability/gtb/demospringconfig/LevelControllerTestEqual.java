73e2aa65a685ad7ac0adab5f6de5f243877076fd
Jianan.Li <jianan.li@thoughtworks.com>
2020-10-26 18:41:50 +0800

    solution 3 for problem 2

--- src/test/java/com/thoughtworks/capability/gtb/demospringconfig/LevelControllerTestEqual.java
+++ src/test/java/com/thoughtworks/capability/gtb/demospringconfig/LevelControllerTestEqual.java
@@ -5,16 +5,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(properties = {"levelNumber=1"}, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
-@TestPropertySource(properties = {"levelNumber=1"})
 public class LevelControllerTestEqual {
     @Autowired
     private MockMvc mockMvc;
--- src/test/java/com/thoughtworks/capability/gtb/demospringconfig/LevelControllerTestLess.java
+++ src/test/java/com/thoughtworks/capability/gtb/demospringconfig/LevelControllerTestLess.java
@@ -5,15 +5,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(properties = {"levelNumber=0"}, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
-@TestPropertySource(properties = {"levelNumber=0"})
 public class LevelControllerTestLess {
     @Autowired
     private MockMvc mockMvc;
