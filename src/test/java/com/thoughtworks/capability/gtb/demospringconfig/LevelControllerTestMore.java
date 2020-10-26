43e9c97d2a5ef5162dcc4861ab05a469cc243abf
Jianan.Li <jianan.li@thoughtworks.com>
2020-10-26 18:32:04 +0800

    solution 1 for problem 2

---
+++ src/test/java/com/thoughtworks/capability/gtb/demospringconfig/LevelControllerTestMore.java
@@ -0,0 +1,29 @@
+package com.thoughtworks.capability.gtb.demospringconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+@TestPropertySource("classpath:testLevelNumberMore.properties")
+public class LevelControllerTestMore {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void should_return_advanced_when_levelNumber_is_more_than_1() throws Exception {
+        mockMvc.perform(get("/level")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string("advanced"));
+    }
+}
